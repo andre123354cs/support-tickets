398f8dcea6e735b8255ce464aa5b32414cf91cc7
andre123354cs <edwar.vaco@gmail.com>
2025-01-21 08:40:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,17 @@ st.set_page_config(
     )
 
 
-output_path = r"https://drive.usercontent.google.com/u/0/uc?id=1ZBk8wgXGnyTGZKQxNQEmRTmpzTlqa9KO&export=download"
-df = pd.read_csv(output_path)
+url_carteras = {
+        "Comfama": r"https://drive.usercontent.google.com/u/0/uc?id=1ZBk8wgXGnyTGZKQxNQEmRTmpzTlqa9KO&export=download",
+        "Azzorti": r"https://drive.usercontent.google.com/u/0/uc?id=1kxVQ7PvD9ueO8qqZdk9aZotYGVjiRq0Y&export=download",
+        "Cueros": r"https://drive.usercontent.google.com/u/0/uc?id=1ffZu1qoNK5iBo7lSo8WbS1bKZRYaIbMK&export=download",
+        "Keypagos" : r"https://drive.usercontent.google.com/u/0/uc?id=1zYX2uONwvxBolHPEl4pm3qVjFeTRXCrQ&export=download",
+        "Linea Directa": r"https://drive.usercontent.google.com/u/0/uc?id=1ceYiVkmia5GvS1m0TYuv9tdQ00UjhP1A&export=download",
+        "Nova Mexico": r"https://drive.usercontent.google.com/u/0/uc?id=1Z8UOXpetXRvDPala0rSzPlQh8tSwbvIA&export=download",
+        "Nova_Colombia": r"https://drive.usercontent.google.com/u/0/uc?id=1EkGd1UbyuVpCK2yqDjrxzV1i6SLNi607&export=download",
+        #"Dolce": r"https://drive.usercontent.google.com/u/0/uc?id=1Ew7xK8rjDGDA4jjkhQ_XGRfDuFzIG5e8&export=download",
+    }
+
 
 st.markdown(""" <div style='text-align: center; display: flex; align-items: center; justify-content: center;'> <h1 style='color: #005780; font-size: 40px;'>INVERSIONES JAY MORRIS SAS </h1> <img src='https://i0.wp.com/ijmsas.com/wp-content/uploads/2021/09/logo-ijm.png?resize=2048%2C2024&ssl=1' style='margin-left: 20px;' width='128' height='128'> </div> """, unsafe_allow_html=True)
 
