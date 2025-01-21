417e14905f83a913cc550caa5e695bad669dc7e8
andre123354cs <edwar.vaco@gmail.com>
2025-01-21 09:15:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,32 +20,34 @@ st.markdown("""
 
 tab1, tab2 = st.tabs(["Promedios", "Existencias"])
 
-
-
 with tab1:
+    st.markdown("<div style='text-align: center;'><h1 style='color: #005780; font-size: 30px;'>PROMEDIOS</h1></div>", unsafe_allow_html=True)
+    gsheetid = '187dwusJTD2uEMOFNQb8735wvbj2cVSbaE_MytHQYdU0'
+    sheetod = '2086518071'
+    url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
+    dfDatos = pd.read_csv(url)
+
+    st.dataframe(dfDatos)
+
+with tab2:
     st.markdown("<div style='text-align: center;'><h1 style='color: #005780; font-size: 30px;'>EXISTENCIAS</h1></div>", unsafe_allow_html=True)
-    gsheetid2 = '187dwusJTD2uEMOFNQb8735wvbj2cVSbaE_MytHQYdU0'
-    sheetod2 = '2086518071'
+    gsheetid2 = 'another_google_sheet_id'
+    sheetod2 = 'another_sheet_id'
     url2 = f'https://docs.google.com/spreadsheets/d/{gsheetid2}/export?format=csv&gid={sheetod2}&format'
     dfExistencias = pd.read_csv(url2)
 
     empresa_options = dfExistencias['Empresa'].unique().tolist()
     nombre_documento_options = dfExistencias['Nombre_Documento'].unique().tolist()
-    prefijo_options = dfExistencias['prefijo'].unique().tolist()
+    prefijo_options = dfExistencias['Prefijo'].unique().tolist()
 
-    selected_empresa = st.select('Empresa', empresa_options)
-    selected_nombre_documento = st.select('Nombre Documento', nombre_documento_options)
-    selected_prefijo = st.select('Prefijo', prefijo_options)
+    selected_empresas = st.multiselect('Empresa', empresa_options)
+    selected_nombre_documentos = st.multiselect('Nombre Documento', nombre_documento_options)
+    selected_prefijos = st.multiselect('Prefijo', prefijo_options)
 
     filtered_df = dfExistencias[
-        (dfExistencias['Empresa'] == selected_empresa) &
-        (dfExistencias['Nombre_Documento'] == selected_nombre_documento) &
-        (dfExistencias['prefijo'] == selected_prefijo)
+        (dfExistencias['Empresa'].isin(selected_empresas)) &
+        (dfExistencias['Nombre_Documento'].isin(selected_nombre_documentos)) &
+        (dfExistencias['Prefijo'].isin(selected_prefijos))
     ]
 
     st.dataframe(filtered_df)
-
-
-with tab2:
-    st.markdown("<div style='text-align: center;'><h1 style='color: #005780; font-size: 30px;'>PROMEDIOS</h1></div>", unsafe_allow_html=True)
-   
