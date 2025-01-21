30453ed7603ada0cd055ca2df7572b21f8f87aa4
andre123354cs <edwar.vaco@gmail.com>
2025-01-21 09:04:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pyarrow.parquet as pq
-import requests
 
 st.set_page_config(
     page_title="MetaData",
@@ -10,26 +8,26 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-st.markdown("""
-<div style='text-align: center; display: flex; align-items: center; justify-content: center;'>
-    <h1 style='color: #005780; font-size: 40px;'>INVERSIONES JAY MORRIS SAS</h1>
-    <img src='https://i0.wp.com/ijmsas.com/wp-content/uploads/2021/09/logo-ijm.png?resize=2048%2C2024&ssl=1' style='margin-left: 20px;' width='128' height='128'>
-</div>
-""", unsafe_allow_html=True)
-
+gsheetid = '187dwusJTD2uEMOFNQb8735wvbj2cVSbaE_MytHQYdU0'
+sheetod = '2086518071'
+url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
+dfDatos = pd.read_csv(url)
 
 tab1, tab2 = st.tabs(["Promedios", "Existencias"])
 
 with tab1:
     st.markdown("<div style='text-align: center;'><h1 style='color: #005780; font-size: 30px;'>PROMEDIOS</h1></div>", unsafe_allow_html=True)
-    gsheetid = '187dwusJTD2uEMOFNQb8735wvbj2cVSbaE_MytHQYdU0'
-    sheetod = '2086518071'
-    url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
-    dfDatos = pd.read_csv(url)
-
     st.dataframe(dfDatos)
 
+    autonumerico_values = dfDatos['autonumerico'].unique()
+    selected_value = st.selectbox('Selecciona un valor de autonumerico:', autonumerico_values)
     
+    filtered_df = dfDatos[dfDatos['autonumerico'] == selected_value]
+    st.dataframe(filtered_df)
 
 with tab2:
     st.markdown("<div style='text-align: center;'><h1 style='color: #005780; font-size: 30px;'>EXISTENCIAS</h1></div>", unsafe_allow_html=True)
+    st.write("Here you can add the logic for the 'Existencias' tab")
+
+
+
